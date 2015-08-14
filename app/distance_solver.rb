2edbd8a543f191493935a91e9d17bca5775ff036
Nicholas Kirchner <nicholas.r.kirchner.1@gmail.com>
2015-08-13 21:58:11 -0700

    Do some backtracing to determine minimal distance

--- app/distance_solver.rb
+++ app/distance_solver.rb
@@ -7,22 +7,31 @@ class WikipediaDistanceSolver
   end
 
   def find_distance
-    until @new_nodes_by_tier.last.empty? || @all_nodes_seen.include?(@end_point)
+    return 0 if @start_point == @end_point
+    backtrace_one_level!
+    return 1 if @one_tier_from_done.include?(@end_point)
+    
+    until @new_nodes_by_tier.last.empty? || @all_nodes_seen.intersect?(@one_tier_from_done)
       get_next_tier!
-      puts "tier: #{@new_nodes_by_tier.count}, items: #{@new_nodes_by_tier.last.count}"
     end
-    @new_nodes_by_tier.empty? ? -1 : @new_nodes_by_tier.count - 1
+    @new_nodes_by_tier.empty? ? -1 : @new_nodes_by_tier.count
   end
 
   def get_next_tier!
     next_tier = Set.new
     @new_nodes_by_tier.last.each do |new_node|
       getter = WikipediaGetter.new(new_node)
-      next_tier.merge(getter.get_linked_page_titles)
-      break if next_tier.include? @end_point
+      new_items = Set.new(getter.get_linked_page_titles)
+      next_tier.merge(new_items)
+      break if new_items.intersect?(@one_tier_from_done)
     end
     @new_nodes_by_tier << next_tier - @all_nodes_seen
     @all_nodes_seen.merge(@new_nodes_by_tier.last)
+  end
+
+  def backtrace_one_level!
+    getter = WikipediaGetter.new(@end_point)
+    @one_tier_from_done = Set.new(getter.get_backlinked_page_titles)
   end
 
 end
